37436694832991032f1ce83518c9d815092eb7c7
nerdycat99 <info@drysky.co.uk>
2017-11-26 10:22:20 +0000

    added verification checks thaat a user has enrolled in a course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_lesson, only: [:show]
 
 	def show
 	end
 
 	private
 
+  def require_enrolled_in_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course) , alert: "sorry you'll need to enrol in this course before viewing a lesson." 
+    end
+  end
+
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
 end
+
